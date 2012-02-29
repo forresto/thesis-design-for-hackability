3f8c79a1ebd53d089be649164f747545e30654dc
Forrest Oliphant <github.com@sembiki.com>
2012-02-29 21:15:41 +0200

    notes for sections, pdf

---
+++ children/define-inputs-outputs-example.js
@@ -0,0 +1,29 @@
+Meemoo
+  .setInfo({ 
+    title: "example", 
+    author: "forresto", 
+    description: "this script defines a Meemoo module" 
+  })   
+  .addInputs({ 
+    square: { 
+      action: function (n) { 
+        Meemoo.send("squared", n*n); 
+      }, 
+      type: "number" 
+    }, 
+    reverse: { 
+      action: function (s) { 
+        var reversed = s.split("").reverse().join(""); 
+        Meemoo.send("reversed", reversed); 
+      }, 
+      type: "string" 
+    } 
+  }) 
+  .addOutputs({ 
+    squared: { 
+      type: "number" 
+    }, 
+    reversed: { 
+      type: "string" 
+    } 
+  });
